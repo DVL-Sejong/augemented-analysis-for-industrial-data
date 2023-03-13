042c648087d0a8549b770a8e6d642f63ee8f61de
usangbong <49114064+usangbong@users.noreply.github.com>
2023-03-13 13:21:21 +0900

    Add files via upload

--- Data-Visualization/Node-edge Visualization/Tools/image/app.py
+++ Data-Visualization/Node-edge Visualization/Tools/image/app.py
@@ -1,149 +1,101 @@
-import os
-import math
-
-import numpy as np
-import pandas as pd
-
 import cv2
-import seaborn as sns
 
-STI_DIR = "./stimuli/"
-STI_LIST = os.listdir(STI_DIR)
-OUT_DIR = "./segmentation/"
-
-CLICKS = []
-AREAS = []
-IMAGE = 0
-mouse_x, mouse_y = -1, -1
-mouse_down_flag = False
-mouse_move_x, mouse_move_y = -1, -1
-STIMULUS_INDEX = 0
-
-PALETTE = sns.color_palette(None, 50)
-palette_index = 0
-for _color_float in PALETTE:
-  _c_int_255 = (int(_color_float[0]*255), int(_color_float[1]*255), int(_color_float[2]*255))
-  PALETTE[palette_index] = _c_int_255
-  palette_index = palette_index+1
-
-def mouse_callback(event, x, y, flags, param):
-  global CLICKS, mouse_x, mouse_y, mouse_down_flag, mouse_move_x, mouse_move_y
-  if event == cv2.EVENT_LBUTTONUP:
-    mouse_x, mouse_y = x, y
-    # print("("+str(mouse_x)+", "+str(mouse_y)+")")
-    CLICKS.append([mouse_x, mouse_y])
-    # print(CLICKS)
-    if mouse_down_flag == False:
-      mouse_down_flag = True
-  elif event == cv2.EVENT_MOUSEMOVE:
-    mouse_move_x, mouse_move_y = x, y
-
-  draw_circle()
-
-def draw_circle():
-  global IMAGE
-  if len(AREAS) != 0:
-    colorIdx = 0
-    for area in AREAS:
-      count = 0
-      prevPt = AREAS[0]
-      for point in area:
-        if count != 0:
-          cv2.line(IMAGE, (prevPt[0], prevPt[1]), (point[0], point[1]), PALETTE[colorIdx], 2)
-        cv2.circle(IMAGE, (point[0], point[1]), 4, PALETTE[colorIdx], -1)
-        count = count+1
-        prevPt = point
-      # print(np.array(area))
-      # cv2.drawContours(IMAGE, np.array(area), contourIdx=-1, color=(0,0,255), thickness=-1)
-      cv2.fillPoly(IMAGE, pts=[np.array(area)], color=PALETTE[colorIdx])
-      colorIdx = colorIdx+1
-
-  if len(CLICKS) != 0 and len(CLICKS) < 2:
-    # lastPt = []
-    for point in CLICKS:
-      cv2.circle(IMAGE, (point[0], point[1]), 4, (0,0,255), -1)
-      # lastPt = point
-    # cv2.line(IMAGE, (lastPt[0], lastPt[1]), (mouse_move_x, mouse_move_y), (0,0,255), 2)
-  elif len(CLICKS) >= 2:
-    count = 0
-    prevPt = CLICKS[0]
-    # lastPt = []
-    for point in CLICKS:
-      cv2.circle(IMAGE, (point[0], point[1]), 4, (0,0,255), -1)
-      if count != 0:
-        cv2.line(IMAGE, (prevPt[0], prevPt[1]), (point[0], point[1]), (0,0,255), 2)
-      count = count+1
-      prevPt = point
-      # lastPt = point
-    # cv2.line(IMAGE, (lastPt[0], lastPt[1]), (mouse_move_x, mouse_move_y), (0,0,255), 2)
+def mouse_call_back(event, x, y, flags, param):  
+    if event == cv2.EVENT_LBUTTONDOWN:
+        for i in range(0, len(contours)):         
+            r = cv2.pointPolygonTest(contours[i], (x, y), False)
+            # print(r)
+            if r > 0:
+                print("Selected contour ", i)
 
 
+dots            = cv2.imread('c0+indoor_001.jpg', cv2.IMREAD_COLOR)
+dots_cpy        = cv2.cvtColor(dots, cv2.COLOR_BGR2GRAY)
+(threshold, bw) = cv2.threshold(dots_cpy, 127, 255, cv2.THRESH_BINARY)
+contours, hier  = cv2.findContours(bw, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+contours        = contours[0:-1] # takes out contour bounding the whole image
 
+cv2.namedWindow("res")
+cv2.drawContours(dots, contours, -1, (0, 255, 0), 3)
+for idx, c in enumerate(contours):  # numbers the contours
+    x = int(sum(c[:,0,0]) / len(c))
+    y = int(sum(c[:,0,1]) / len(c))
+    # cv2.putText(dots, str(idx), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+cv2.imshow("res", dots)
+cv2.setMouseCallback('res', mouse_call_back)
 
-sti_path = STI_DIR+STI_LIST[STIMULUS_INDEX]
-IMAGE = cv2.imread(sti_path)
-while True:
-  cv2.imshow("iamge", IMAGE)
-  cv2.setMouseCallback("iamge", mouse_callback, IMAGE)
-  key = cv2.waitKey(1)
-  if key == 27:
-    break
-  elif key == ord('a'):
-    IMAGE = cv2.imread(sti_path)
-    if len(CLICKS) < 3:
-      continue
-    area = []
-    fPt = CLICKS[0]
-    for pt in CLICKS:
-      area.append([pt[0], pt[1]])
-    area.append([fPt[0], fPt[1]])
-    AREAS.append(area)
+cv2.waitKey()
 
-    CLICKS = []
-    if mouse_down_flag == True:
-      mouse_down_flag = False
-    continue
-  elif key == ord('n'):
-    if len(STI_LIST) > STIMULUS_INDEX+1:
-      # segmentation data save process
-      img_height, img_width = IMAGE.shape[:2]
-      areaCount = 1
-      allPixels = np.zeros(IMAGE.shape[:2])
-      for area in AREAS:
-        for _r in range(0, img_height):
-          for _c in range(0, img_width):
-            if allPixels[_r][_c] == 0:
-              innerCheck = cv2.pointPolygonTest(np.array(area), (_c, _r), False)
-              if innerCheck >= 0:
-                allPixels[_r][_c] = areaCount
-                # print("("+str(_c)+", "+str(_r)+")")
-        areaCount = areaCount+1
-      allPixels_df = pd.DataFrame(allPixels, columns=None)
-      out_path = OUT_DIR+STI_LIST[STIMULUS_INDEX].split('.')[0]+".csv"
-      allPixels_df.to_csv(out_path, index=False, header=False)
-      print(out_path)
-      AREAS = []
-      CLICKS = []
-
-      STIMULUS_INDEX = STIMULUS_INDEX+1
-      sti_path = STI_DIR+STI_LIST[STIMULUS_INDEX]
-      IMAGE = cv2.imread(sti_path)
-    continue
-  elif key == ord('b'):
-    if STIMULUS_INDEX > 0:
-      STIMULUS_INDEX = STIMULUS_INDEX-1
-      sti_path = STI_DIR+STI_LIST[STIMULUS_INDEX]
-      IMAGE = cv2.imread(sti_path)
-    continue
-  elif key == ord('r'):
-    IMAGE = cv2.imread(sti_path)
-    if len(CLICKS) >= 1:
-      CLICKS.pop()
-    continue
-  elif key == ord('-'):
-    IMAGE = cv2.imread(sti_path)
-    if len(AREAS) >= 1:
-      AREAS.pop()
-    continue
-
-cv2.destroyAllWindows()
+# import cv2
+ 
+# # Read image for contour detection
+# input_image = cv2.imread("c0+indoor_001.jpg")
+ 
+# # Make a copy to draw bounding box
+# input_image_cpy = input_image.copy()
+ 
+# # Convert input image to grayscale
+# gray_img = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+ 
+# threshold_value = gray_img[216, 402]
+# print(threshold_value)
+ 
+# # Convert the grayscale image to binary (image binarization <a href="https://thinkinfi.com/basic-python-opencv-tutorial-function/" data-internallinksmanager029f6b8e52c="14" title="OpenCV" target="_blank" rel="noopener">opencv</a> <a href="https://thinkinfi.com/learn-python/" data-internallinksmanager029f6b8e52c="13" title="Best way to learn Python" target="_blank" rel="noopener">python</a>)
+# ret, binary_img = cv2.threshold(gray_img, threshold_value, 255, cv2.THRESH_BINARY)
+ 
+# # Invert image
+# inverted_binary_img = ~ binary_img
+ 
+# # Detect contours
+# # hierarchy variable contains information about the relationship between each contours
+# contours_list, hierarchy = cv2.findContours(inverted_binary_img,
+#                                        cv2.RETR_TREE,
+#                                        cv2.CHAIN_APPROX_SIMPLE) # Find contours
+ 
+# # for each detected contours
+# for contour_num in range(len(contours_list)):
+ 
+#     # Draw detected contour with shape name
+#     contour1 = cv2.drawContours(input_image_cpy, contours_list, contour_num, (255, 0, 255), 3)
+ 
+#     # Find number of points of detected contour
+#     end_points = cv2.approxPolyDP(contours_list[contour_num], 0.01 * cv2.arcLength(contours_list[contour_num], True), True)
+ 
+#     # Make sure contour area is large enough (Rejecting unwanted contours)
+#     if (cv2.contourArea(contours_list[contour_num])) > 10000:
+ 
+#         # Find first point of each shape
+#         point_x = end_points[0][0][0]
+#         point_y = end_points[0][0][1]
+ 
+#         # Writing shape name at center of each shape in black color (0, 0, 0)
+#         text_color_black = (0, 0, 0)
+ 
+#         # If a contour have three end points, then shape should be a Triangle
+#         if len(end_points) == 3:
+#             cv2.putText(input_image_cpy, 'Triangle', (point_x, point_y),
+#                         cv2.FONT_HERSHEY_SIMPLEX, 0.9, text_color_black, 2)
+ 
+#         # If a contour have four end points, then shape should be a Rectangle or Square
+#         elif len(end_points) == 4:
+#             cv2.putText(input_image_cpy, 'Rectangle', (point_x, point_y),
+#                         cv2.FONT_HERSHEY_SIMPLEX, 0.9, text_color_black, 2)
+ 
+#         # If a contour have five end points, then shape should be a Pentagon
+#         elif len(end_points) == 5:
+#             cv2.putText(input_image_cpy, 'Pentagon', (point_x, point_y),
+#                         cv2.FONT_HERSHEY_SIMPLEX, 0.9, text_color_black, 2)
+ 
+#         # If a contour have ten end points, then shape should be a Star
+#         elif len(end_points) == 10:
+#             cv2.putText(input_image_cpy, 'Star', (point_x, point_y),
+#                         cv2.FONT_HERSHEY_SIMPLEX, 0.9, text_color_black, 2)
+ 
+#         # If a contour have more than ten end points, then shape should be a Star
+#         else:
+#             cv2.putText(input_image_cpy, 'circle', (point_x, point_y),
+#                         cv2.FONT_HERSHEY_SIMPLEX, 0.9, text_color_black, 2)
+ 
+#     cv2.imshow('First detected contour', contour1)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()

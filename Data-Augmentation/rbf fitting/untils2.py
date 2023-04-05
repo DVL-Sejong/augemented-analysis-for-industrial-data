1dbd36cc70bdc77ed669031cffd4d6e7eb963ef5
cooling-0 <ekzmsltm6560@naver.com>
2023-04-05 16:54:04 +0900

    add commti

---
+++ Data-Augmentation/rbf fitting/untils2.py
@@ -0,0 +1,10 @@
+class MakeDataset(Dataset):
+    def __init__(self, x_tensor, y_tensor):
+        self.x = x_tensor
+        self.y = y_tensor
+
+    def __getitem__(self, index):
+        return (self.x[index], self.y[index])
+
+    def __len__(self):
+        return len(self.x)

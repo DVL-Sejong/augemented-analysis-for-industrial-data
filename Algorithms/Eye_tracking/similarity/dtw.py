936221e217407393e7fe05357af12c422d056300
usangbong <49114064+usangbong@users.noreply.github.com>
2023-04-28 16:45:00 +0900

    Update dtw.py

--- Algorithms/Eye_tracking/similarity/dtw.py
+++ Algorithms/Eye_tracking/similarity/dtw.py
@@ -7,3 +7,9 @@ from fastdtw import fastdtw
 x = np.array([[0, 2], [2, 6], [11, 4], [9, 9]])
 y = np.array([[3, 2], [5, 6], [14, 4], [12, 9]])
 z = np.array([[2, 2], [0, 8], [9, 3], [9, 11]])
+
+distance, path = fastdtw(x, y, dist=euclidean)
+print(distance)
+
+distance, path = fastdtw(x, z, dist=euclidean)
+print(distance)

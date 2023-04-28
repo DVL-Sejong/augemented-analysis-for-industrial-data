a9e1770e59b5efdefd8d95f52c8fc1196c52851e
usangbong <49114064+usangbong@users.noreply.github.com>
2023-04-28 16:44:52 +0900

    Update dtw.py

--- Algorithms/Eye_tracking/similarity/dtw.py
+++ Algorithms/Eye_tracking/similarity/dtw.py
@@ -2,3 +2,8 @@ import numpy as np
 from scipy.spatial.distance import euclidean
 
 from fastdtw import fastdtw
+
+
+x = np.array([[0, 2], [2, 6], [11, 4], [9, 9]])
+y = np.array([[3, 2], [5, 6], [14, 4], [12, 9]])
+z = np.array([[2, 2], [0, 8], [9, 3], [9, 11]])

9807b7095123c5597a88ccf3e95b73e5c95810ef
Seongbum Seo <seo@seongbum.com>
2024-08-02 08:10:39 +0900

    Anomaly detection

--- Anomaly-Detection/analyzer.py
+++ Anomaly-Detection/analyzer.py
@@ -9,3 +9,15 @@ import numpy as np
 from pandas_analysis import get_attributes_from_flow_list
 
 # from blacklist_update import Blacklist
+
+_FLOW_FIELDS = [
+    "ts",
+    "ip_protocol",
+    "state",
+    "src_ip",
+    "src_port",
+    "dst_ip",
+    "dst_port",
+    "src_tx",
+    "dst_tx",
+]

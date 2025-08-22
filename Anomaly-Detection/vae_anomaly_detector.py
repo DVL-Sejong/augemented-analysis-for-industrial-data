30cb1b3402d13a0fac4ff8a04de57a889dbaad11
Seongbum Seo <seo@seongbum.com>
2025-08-22 15:26:03 +0900

    Anomaly detection

--- Anomaly-Detection/vae_anomaly_detector.py
+++ Anomaly-Detection/vae_anomaly_detector.py
@@ -149,3 +149,4 @@ class VAEAnomalyDetector:
     def vae_loss(self, recon_x, x, mu, logvar):
         """VAE 손실 함수"""
         BCE = nn.functional.binary_cross_entropy(recon_x, x, reduction='sum')
+        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())

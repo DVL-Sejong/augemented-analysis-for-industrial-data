6b9dc3e6c2a16e182cf00ad30c4e7df8cbcaa022
Seongbum Seo <seo@seongbum.com>
2025-10-24 16:09:35 +0900

    Anomaly detection

--- Anomaly-Detection/vae_anomaly_detector.py
+++ Anomaly-Detection/vae_anomaly_detector.py
@@ -285,3 +285,4 @@ class VAEAnomalyDetector:
     def get_anomaly_report(self, flows):
         """이상 탐지 결과 리포트 생성"""
         anomalies, recon_errors = self.predict(flows)
+        total_flows = len(flows)

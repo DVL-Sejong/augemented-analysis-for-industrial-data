10e130cd1d4579a0831730ad31b4f41846e161be
Seongbum Seo <seo@seongbum.com>
2025-11-17 20:27:35 +0900

    Anomaly detection

--- Anomaly-Detection/vae_anomaly_detector.py
+++ Anomaly-Detection/vae_anomaly_detector.py
@@ -296,3 +296,4 @@ class VAEAnomalyDetector:
         report['mean_recon_error'] = float(np.mean(recon_errors))
         report['max_recon_error'] = float(np.max(recon_errors))
         report['min_recon_error'] = float(np.min(recon_errors))
+        return report

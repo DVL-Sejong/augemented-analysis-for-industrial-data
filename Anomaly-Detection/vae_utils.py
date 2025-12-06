2262f501b6c50cd5ff02a4d936062906daf57685
Seongbum Seo <seo@seongbum.com>
2025-12-06 13:53:23 +0900

    Anomaly detection

--- Anomaly-Detection/vae_utils.py
+++ Anomaly-Detection/vae_utils.py
@@ -2,3 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from typing import List, Tuple, Dict
+
+
+def calculate_reconstruction_error_statistics(recon_errors: np.ndarray) -> Dict[str, float]:

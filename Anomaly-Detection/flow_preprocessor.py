faa36fe4a9fc1439c32695edcba6669ebeb75293
Seongbum Seo <seo@seongbum.com>
2025-12-15 22:54:57 +0900

    Anomaly detection

--- Anomaly-Detection/flow_preprocessor.py
+++ Anomaly-Detection/flow_preprocessor.py
@@ -6,3 +6,4 @@ from typing import List, Optional
 class FlowPreprocessor:
     """네트워크 플로우 데이터 전처리 클래스"""
     def __init__(self, scaler_type: str = 'standard'):
+        self.scaler_type = scaler_type
